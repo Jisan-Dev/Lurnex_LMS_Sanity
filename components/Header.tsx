de02013fecca4f9455644af2a2d76591f6b52a84
Jisun696 <istiakkashem35@gmail.com>
2025-04-24 23:03:46 +0600

    code the course progress component

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,9 @@ export default function Header() {
               // prefetch={false}
               className="flex items-center space-x-2 hover:opacity-90 transition-opacity">
               <BookOpen className="h-6 w-6 text-primary" />
-              <span className="text-xl font-bold bg-gradient-to-r from-primary/90 to-primary bg-clip-text text-transparent">Lurnex</span>
+              <span className="text-xl font-bold bg-gradient-to-r from-primary/90 to-primary bg-clip-text text-transparent">
+                Lurnex
+              </span>
             </Link>
 
             <SearchInput />
